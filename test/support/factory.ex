cbb933f6f5214571f2f88b10e8ff17ae82ec386f
Francine Guimarães <francinesguimaraes@outlook.com>
2021-10-10 20:20:09 -0300

    test: refactor item factory

--- test/support/factory.ex
+++ test/support/factory.ex
@@ -5,17 +5,24 @@ defmodule Exlivery.Factory do
   alias Exlivery.Types.Order
 
   def user_factory do
-    %{birthdate: "1991/09/09", cpf: "09876535362", email: "amy@nbc.com", id: "25cb98a6-9879-4af1-a423-5ceb840be600", name: "Amy Santiago"}
+    %{
+      birthdate: "1991/09/09",
+      cpf: "09876535362",
+      email: "amy@nbc.com",
+      id: "25cb98a6-9879-4af1-a423-5ceb840be600",
+      name: "Amy Santiago"
+    }
   end
 
   def item_factory do
-    Item.build(
-      "X-Burger",
-      1,
-      25.00,
-      "Vegetarian hamburger made with fresh beens, tomatoes and chickpea",
-      "without mayo"
-    )
+    %{
+      description: "Vegetarian hamburger made with fresh beens, tomatoes and chickpea",
+      details: "without mayo",
+      id: "27aaf001-ec50-45de-82f6-a976f7501547",
+      name: "X-Burger",
+      quantity: 1,
+      unity_price: Decimal.new("25.00")
+    }
   end
 
   def order_factory do
